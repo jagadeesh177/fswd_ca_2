a9c467c3e0b2ad8a33953749c08330a0cb60acb0
jagadeesh177 <you@example.com>
2025-11-03 14:46:09 +0530

    added

--- Travel/src/Components/Blog.jsx
+++ Travel/src/Components/Blog.jsx
@@ -4,7 +4,12 @@ function Blog() {
 
   return (
   <>
-  
+  <h3>Blog Post</h3>
+  <ol>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet consectetur.</li>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+  </ol>
   </>
   )
 }
