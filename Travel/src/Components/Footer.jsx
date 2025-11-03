b429149c23b985ee0b7729b97b6611fc7e5736b6
jagadeesh177 <you@example.com>
2025-11-03 14:23:56 +0530

    added

--- Travel/src/Components/Footer.jsx
+++ Travel/src/Components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
         <h2>Contact us</h2>
-        <p><strong>Email:</strong>travelblog@gmail.com</p>
+        <p><strong>Email: </strong>travelblog@gmail.com</p>
         <p><strong>Mobile No: +91 6301418455</strong></p>
     </footer>
   )
