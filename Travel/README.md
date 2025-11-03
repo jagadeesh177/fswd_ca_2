88e44aedf9020419ae6834ef62c20c03797813f1
jagadeesh177 <you@example.com>
2025-11-03 14:20:13 +0530

    update

--- Travel/README.md
+++ Travel/README.md
@@ -1,3 +1,3 @@
 # fswd_ca_2
 GitHub Repo link: https://github.com/jagadeesh177/fswd_ca_2.git
-Deployment link: 
+Deployment link: https://fswd-ca-2-d72r.vercel.app/
--- Travel/src/Components/Footer.jsx
+++ Travel/src/Components/Footer.jsx
@@ -6,6 +6,7 @@ function Footer() {
     <footer>
         <h1>Contact us</h1>
         <p><strong>Email:</strong>travelblog@gmail.com</p>
+        <p><strong>Mobile No: +91 6301418455</strong></p>
     </footer>
   )
 }
